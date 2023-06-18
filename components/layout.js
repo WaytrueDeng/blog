09e34875dd71c2f9628e0a091c2e043f1ff8292c
Your Name <you@example.com>
2023-06-18 19:43:41 +0800

    responsive prettifer

--- components/layout.js
+++ components/layout.js
@@ -2,5 +2,5 @@ import Link from "next/link";
 export default function Layout({ children,backLinks,title }) {
   const backLinktoslug = backLinks ? backLinks.map((item) => item.replace(/\.md$/g,"" )) : [];
   const backLinksFragment = backLinktoslug.map((backlink) => <li key={backlink}><Link href={backlink} >{backlink}</Link></li>);
-  return (<><div className="nav"><h1>{title}</h1></div><div className="ml-[20vw] mr-[2vw] ">{children}</div><div className="backlink">Go <Link href="/posts/home">home</Link><br /><ul>backlinks {backLinksFragment}</ul></div></>);
+  return (<><div className="nav"><h1>{title}</h1></div><div className="container">{children}<div className="backlink">Go <Link href="/posts/home">home</Link><br /><ul>backlinks {backLinksFragment}</ul></div></div></>);
 }
